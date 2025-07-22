cd85f3b9210d0e09e2e2ca840f3fb9cb46d71834
ppuivif <papuivif@gmail.com>
2025-07-22 19:32:30 +0200

    2025-07-22

--- Exam_rank_05/CPP02/ASpell.cpp
+++
@@ -1,7 +0,0 @@
-#include "ASpell.hpp"
-#include "ATarget.hpp"
-
-void ASpell::launch(ATarget const & target) const{
-
-	target.getHitBySpell(*this);
-}
--- Exam_rank_05/CPP02/ATarget.cpp
+++
@@ -1,7 +0,0 @@
-#include "ATarget.hpp" 
-#include "ASpell.hpp"
-
-void ATarget::getHitBySpell(ASpell const & spell) const{
-
-	std::cout << this->_type << " has been " << spell.getEffects() << "!" << std::endl;
-}
